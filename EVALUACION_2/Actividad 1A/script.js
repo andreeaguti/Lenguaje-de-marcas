43711d86baaba6cf9ea52193364e194335301952
COMPUS029\daw1 <agutierrezp18@educantabria.es>
2026-01-14 13:39:38 +0100

    lnh

---
+++ EVALUACION_2/Actividad 1A/script.js
@@ -0,0 +1,30 @@
+var boton1 = document.getElementById('boton1');
+var boton2 = document.getElementById('boton2');
+var boton3 = document.getElementById('boton3');
+var boton4 = document.getElementById('boton4');
+var boton5 = document.getElementById('boton5');
+var boton6 = document.getElementById('boton6');
+var boton7 = document.getElementById('boton7');
+var boton8 = document.getElementById('boton8');
+var boton9 = document.getElementById('boton9');
+var boton10 = document.getElementById('boton10');
+var boton11 = document.getElementById('boton11');
+var boton12 = document.getElementById('boton12');
+var boton13 = document.getElementById('boton13');
+
+
+var p1 = document.getElementById('mensaje');
+
+console.log('¡El archivo JavaScript está funcionando!'); 
+
+console.log(mensaje); 
+
+boton1.addEventListener('click',()=> {
+    console.log('¡Buenos días!');
+    p1.textContent ='Buenos dias'
+ });
+
+ boton2.addEventListener('click',()=> {
+    console.log('¡Buenos días!');
+ });
+
