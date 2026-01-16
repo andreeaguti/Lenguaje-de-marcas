a4cefce657556816d0e8edb3ba60598a48c03b66
COMPUS029\daw1 <agutierrezp18@educantabria.es>
2026-01-16 11:36:37 +0100

    primer ejercicio

--- EVALUACION_2/Actividad 1A/script.js
+++ EVALUACION_2/Actividad 1A/script.js
@@ -1,30 +1,35 @@
-var boton1 = document.getElementById('boton1');
-var boton2 = document.getElementById('boton2');
-var boton3 = document.getElementById('boton3');
-var boton4 = document.getElementById('boton4');
-var boton5 = document.getElementById('boton5');
-var boton6 = document.getElementById('boton6');
-var boton7 = document.getElementById('boton7');
-var boton8 = document.getElementById('boton8');
-var boton9 = document.getElementById('boton9');
-var boton10 = document.getElementById('boton10');
-var boton11 = document.getElementById('boton11');
-var boton12 = document.getElementById('boton12');
-var boton13 = document.getElementById('boton13');
+console.log("Mi script funciona"); 
+
+const boton1 = document.getElementById('boton1');
+const boton2 = document.getElementById('boton2');
+const boton3 = document.getElementById('boton3');
+const boton4 = document.getElementById('boton4');
+const boton5 = document.getElementById('boton5');
+const boton6 = document.getElementById('boton6');
+const boton7 = document.getElementById('boton7');
+const boton8 = document.getElementById('boton8');
+const boton9 = document.getElementById('boton9');
+const boton10 = document.getElementById('boton10');
+const boton11 = document.getElementById('boton11');
+const boton12 = document.getElementById('boton12');
+const boton13 = document.getElementById('boton13');
 
 
 var p1 = document.getElementById('mensaje');
-
-console.log('¡El archivo JavaScript está funcionando!'); 
 
 console.log(mensaje); 
 
 boton1.addEventListener('click',()=> {
     console.log('¡Buenos días!');
-    p1.textContent ='Buenos dias'
+    p1.textContent ='Buenos dias Andrea';
  });
 
  boton2.addEventListener('click',()=> {
     console.log('¡Buenos días!');
+    var ladocuadrado = 5;
+    var resultadocuadrado = ladocuadrado * ladocuadrado;
+    p1.textContent = 'El area del cuadrado es 
+    '
+
  });
 
